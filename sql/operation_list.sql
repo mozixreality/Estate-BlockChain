98df1658b2a182779c5ad23584607de25c4d94f8
mozixreality <mozixreality@gmail.com>
2022-07-22 09:51:52 +0800

    0722 backup

--- sql/operation_list.sql
+++ sql/operation_list.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:3306
--- 產生時間： 2022 年 06 月 24 日 09:44
+-- 產生時間： 2022 年 07 月 22 日 09:49
 -- 伺服器版本： 8.0.29-0ubuntu0.20.04.3
 -- PHP 版本： 7.4.3
 
@@ -39,9 +39,9 @@ CREATE TABLE `operation_list` (
 
 INSERT INTO `operation_list` (`operation_id`, `operation_type`) VALUES
 (1, 0),
-(2, 0),
-(3, 2),
-(4, 1);
+(2, 2),
+(3, 1),
+(4, 0);
 
 --
 -- 已傾印資料表的索引
